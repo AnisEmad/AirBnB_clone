f62e9e0b077e16d05aefcc236cf08b0249813da5
Yahia <yn7599@gmail.com>
2023-12-08 17:34:21 +0200

    Added BaseModel features

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,29 @@
-#!/usr/bin/python3
-"""Model of the base parent class all classes inherit from"""
+"""Model of the base parent class that all classes inherit from."""
 from models import storage
-import date
+import datetime
 import uuid
 
 class BaseModel:
     """Container of all common attributes/methods for other classes."""
     def __init__(self, *args, **kwargs):
         """Make an instance of the BaseModel."""
-        pass
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """Represent the object in a human readable format."""
-        pass
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def to_dict(self):
         """Get a dictionary containing all attributes of instance."""
-        pass
+        instance_dict = self.__dict__.copy()
+        instance_dict["__class__"] = self.__class__.__name__
+        instance_dict["created_at"] = instance_dict["created_at"].isoformat()
+        instance_dict["updated_at"] = instance_dict["updated_at"].isoformat()
+        return instance_dict
 
     def save(self):
         """Save the attributes at the current time."""
-        pass
+        self.updated_at = datetime.datetime.now()
         # call storage.save()
