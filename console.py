19db75840a48fa4c946a34ae3248863f08fd400c
Yahia <yn7599@gmail.com>
2023-12-10 22:50:56 +0200

    console v0.01

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """The entry point to interact with the AirBnB console."""
 import cmd
+import functions as utils
 from models             import storage
 from models.base_model  import BaseModel
 from models.user        import User
@@ -10,6 +11,36 @@ from models.state       import State
 from models.review      import Review
 from models.amenity     import Amenity
 
+class_list = {
+        "basemodel": BaseModel,
+        "user"     : User,
+        "city"     : City,
+        "place"    : Place,
+        "state"    : State,
+        "review"   : Review,
+        "amenity"  : Amenity
+}
+
+def valid_class(name):
+    """Check if the class is valid or print error."""
+    if name == "":
+        print("** class name missing **")
+    elif name.lower() not in class_list:
+        print("** class doesn't exist **")
+    else:
+        return True
+
+def get_instance(class_name, instance_id):
+    """Get an instance from the storage (if exist)"""
+    key = f"{class_list[class_name.lower()].__name__}.{instance_id}"
+    all = storage.all()
+    if instance_id == "":
+        print("** instance id missing **")
+    elif key not in all:
+        print("** no instance found **")
+    else:
+        return all[key]
+    return False
 
 class HBNBCommand(cmd.Cmd):
     """The console."""
@@ -18,17 +49,30 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, arg):
         """create <class name>
         Create a new instance of the given class."""
-        pass
+        if valid_class(arg):
+            instance = class_list[arg.lower()]()
+            print(instance.id)
+
 
     def do_show(self, arg):
         """show <class name> <instance id>
         Show informations about an instance."""
-        pass
+        args = arg.split(" ") + [""]
+        if valid_class(args[0]):
+            instance = get_instance(args[0], args[1])
+            if instance:
+                print(instance)
+
+
 
     def do_destroy(self, arg):
         """destroy <instance>
         Delete the given instance."""
-        pass
+        args = arg.split(" ") + [""]
+        if valid_class(args[0]):
+            instance = get_instance(args[0], args[1])
+            if instance:
+                del instance
 
     def do_all(self, arg):
         """all [class name]
